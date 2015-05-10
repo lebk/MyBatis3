00d0a1167db180772f927a5dc905c105b2bdae3a
lebk <lebk.lei@gmail.com>
2015-05-10 16:09:57 +0800

    initial checking!

---
+++ jpa_mybatis/src/main/java/jpa_mybatis/AppMain.java
@@ -0,0 +1,8 @@
+package jpa_mybatis;
+/*
+ *Author: leibo(87535204@qq.com
+ *@Date: 2015Äê5ÔÂ10ÈÕ
+ */
+public class AppMain {
+
+}
---
+++ jpa_mybatis/src/test/java/jpa_mybatis/AppMainTest.java
@@ -0,0 +1,36 @@
+package jpa_mybatis;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/*
+ *Author: leibo(87535204@qq.com
+ *@Date: 2015Äê5ÔÂ10ÈÕ
+ */
+public class AppMainTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		// fail("Not yet implemented");
+	}
+
+}
